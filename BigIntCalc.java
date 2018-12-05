8edde0d47f7783950a99bf807eecac0e3f9c16b3
shumatpf <shumatpf@dukes.jmu.edu>
2018-12-05 17:13:53 -0500

    change spacing and class name

--- BigIntCalc.java
+++ BigIntCalc.java
@@ -1,4 +1,5 @@
-public class Test {
+public class BigIntCalc {
+
 	public static void main(String[] args) {
 		if (args.length != 2) {
 			System.out.println("Please enter two integers");
@@ -27,11 +28,11 @@ public class Test {
 		int[] smallAr;
 		int[] sumAr;
 		if (str1.length() >= str2.length()) {
-				bigAr = strIntAr(str1);
-				smallAr = strIntAr(str2);
+			bigAr = strIntAr(str1);
+			smallAr = strIntAr(str2);
 		} else {
-				bigAr = strIntAr(str2);
-				smallAr = strIntAr(str1);
+			bigAr = strIntAr(str2);
+			smallAr = strIntAr(str1);
 		}
 		sumAr = arrPad(bigAr, 1);
 
@@ -47,149 +48,125 @@ public class Test {
 		int remIndex = bigAr.length - smallAr.length;
 
 		while (rem != 0 && remIndex >= 0) {
-				int sum = bigAr[remIndex] + rem;
-				int digit = sum % 10;
-				rem = sum / 10;
+			int sum = bigAr[remIndex] + rem;
+			int digit = sum % 10;
+			rem = sum / 10;
 
-				sumAr[remIndex] = digit;
-				remIndex--;
-
+			sumAr[remIndex] = digit;
+			remIndex--;
 		}
 		sumAr[0] = rem;
 
 		return removeZero(sumAr);
-
 	}
 
 	public static String subtract(String str1, String str2) {
+		int[] intAr1 = strIntAr(str1);
+		int[] intAr2 = strIntAr(str2);
 
-			int[] intAr1 = strIntAr(str1);
-			int[] intAr2 = strIntAr(str2);
+		boolean borrow = false;
 
-			boolean borrow = false;
-
-			for (int i = intAr1.length - 1, j = intAr2.length - 1; j >= 0; i--, j--) {
-					int bI = i;
-					intAr1[i] -= intAr2[j];
-					while (intAr1[bI] < 0) {
-							intAr1[bI] += 10;
-							intAr1[--bI]--;
-					}
+		for (int i = intAr1.length - 1, j = intAr2.length - 1; j >= 0; i--, j--) {
+			int bI = i;
+			intAr1[i] -= intAr2[j];
+			while (intAr1[bI] < 0) {
+				intAr1[bI] += 10;
+				intAr1[--bI]--;
 			}
-			return removeZero(intAr1);
+		}
+		return removeZero(intAr1);
 	}
 
 	public static String multiply(String str1, String str2) {
+		int[] bigAr;
+		int[] smallAr;
+		int[] prodAr;
 
-			int[] bigAr;
-			int[] smallAr;
-			int[] prodAr;
+		if (str1.length() >= str2.length()) {
+			bigAr = strIntAr(str1);
+			smallAr = strIntAr(str2);
+		} else {
+			bigAr = strIntAr(str2);
+			smallAr = strIntAr(str1);
+		}
+		prodAr = new int[smallAr.length + bigAr.length + 1];
 
-			if (str1.length() >= str2.length()) {
-					bigAr = strIntAr(str1);
-					smallAr = strIntAr(str2);
+		int n = prodAr.length - 1;
 
-			} else {
-					bigAr = strIntAr(str2);
-					smallAr = strIntAr(str1);
-
+		for (int i = smallAr.length - 1; i >= 0; i--) {
+			int rem = 0;
+			int x = n;
+			for (int j = bigAr.length - 1; j >= 0; j--) {
+				int product = smallAr[i] * bigAr[j] + rem;
+				int digit = product % 10;
+				rem = product / 10;
+				prodAr[x] += digit;
+				if (prodAr[x] >= 10) {
+					rem = prodAr[x] / 10;
+					prodAr[x] %= 10;
+				}
+				x--;
 			}
-			prodAr = new int[smallAr.length + bigAr.length + 1];
-
-			int n = prodAr.length - 1;
-
-			for (int i = smallAr.length - 1; i >= 0; i--) {
-					int rem = 0;
-					int x = n;
-					for (int j = bigAr.length - 1; j >= 0; j--) {
-							int product = smallAr[i] * bigAr[j] + rem;
-							int digit = product % 10;
-							rem = product / 10;
-							prodAr[x] += digit;
-							if (prodAr[x] >= 10) {
-									rem = prodAr[x] / 10;
-									prodAr[x] %= 10;
-
-							}
-							x--;
-
-					}
-					if (rem != 0) {
-							prodAr[x] += rem;
-							rem = 0;
-
-					}
-					n--;
-
+			if (rem != 0) {
+				prodAr[x] += rem;
+				rem = 0;
 			}
-
-			return removeZero(prodAr);
-
+			n--;
+		}
+		return removeZero(prodAr);
 	}
 
 	public static String divide(String str1, String str2) {
+		int[] ar1 = strIntAr(str1);
+		int[] ar2 = strIntAr(str2);
 
-			int[] ar1 = strIntAr(str1);
-			int[] ar2 = strIntAr(str2);
+		int[] result = new int[ar1.length];
+		int[] initDiv = new int[ar2.length];
 
-			int[] result = new int[ar1.length];
-			int[] initDiv = new int[ar2.length];
+		int divIndex = -1;
 
-			int divIndex = -1;
+		for (int i = 0; i < ar2.length; i++) {
+			initDiv[i] = ar1[i];
+			divIndex++;
+		}
 
-			for (int i = 0; i < ar2.length; i++) {
-					initDiv[i] = ar1[i];
-					divIndex++;
+		try {
+			subtract(removeZero(initDiv), removeZero(ar1));
+		} catch (ArrayIndexOutOfBoundsException e) {
+			initDiv = new int[ar2.length + 1];
 
+			for (int i = 0; i < ar2.length + 1; i++) {
+				initDiv[i] = ar1[i];
+			}
+			divIndex++;
+		}
+
+		String initStr = removeZero(initDiv);
+
+		for (int i = 0; i < ar1.length; i++) {
+			int count = 0;
+
+			String tempInit = new String(initStr);
+
+			while (true) {
+				try {
+					tempInit = subtract(tempInit, removeZero(ar2));
+					count++;
+				} catch (ArrayIndexOutOfBoundsException e) {
+					break;
+				}
 			}
 
-			try {
-					subtract(removeZero(initDiv), removeZero(ar1));
+			result[divIndex++] = count;
 
-			} catch (ArrayIndexOutOfBoundsException e) {
-					initDiv = new int[ar2.length + 1];
-
-					for (int i = 0; i < ar2.length + 1; i++) {
-							initDiv[i] = ar1[i];
-
-					}
-
-					divIndex++;
-
+			if (divIndex == result.length) {
+				break;
 			}
+			String tempProd = multiply(count + "", str2);
 
-			String initStr = removeZero(initDiv);
-
-			for (int i = 0; i < ar1.length; i++) {
-					int count = 0;
-
-					String tempInit = new String(initStr);
-
-					while (true) {
-							try {
-									tempInit = subtract(tempInit, removeZero(ar2));
-									count++;
-
-							} catch (ArrayIndexOutOfBoundsException e) {
-									break;
-
-							}
-
-					}
-
-					result[divIndex++] = count;
-					if (divIndex == result.length) {
-							break;
-
-					}
-					String tempProd = multiply(count + "", str2);
-
-					initStr = subtract(initStr, tempProd) + ar1[divIndex];
-
-			}
-
-			return removeZero(result);
-
+			initStr = subtract(initStr, tempProd) + ar1[divIndex];
+		}
+		return removeZero(result);
 	}
 
 	/**
@@ -199,17 +176,14 @@ public class Test {
 	 * @return int array of string param
 	 */
 	private static int[] strIntAr(String str) {
+		char[] charAr = str.toCharArray();
+		int[] intAr = new int[charAr.length];
 
-			char[] charAr = str.toCharArray();
-			int[] intAr = new int[charAr.length];
+		for (int i = 0; i < charAr.length; i++) {
+			intAr[i] = Character.getNumericValue(charAr[i]);
 
-			for (int i = 0; i < charAr.length; i++) {
-					intAr[i] = Character.getNumericValue(charAr[i]);
-
-			}
-
-			return intAr;
-
+		}
+		return intAr;
 	}
 
 	/**
@@ -220,41 +194,31 @@ public class Test {
 	 * @return padded array
 	 */
 	private static int[] arrPad(int[] arr, int zeros) {
-			int[] newArr = new int[arr.length + zeros];
-			for (int i = 0; i < zeros; i++) {
-					newArr[i] = 0;
-
-			}
-			for (int i = zeros, oldIndex = 0; oldIndex < arr.length; i++, oldIndex++) {
-					newArr[i] = arr[oldIndex];
-
-			}
-
-			return newArr;
-
+		int[] newArr = new int[arr.length + zeros];
+		for (int i = 0; i < zeros; i++) {
+			newArr[i] = 0;
+		}
+		for (int i = zeros, oldIndex = 0; oldIndex < arr.length; i++, oldIndex++) {
+			newArr[i] = arr[oldIndex];
+		}
+		return newArr;
 	}
 
 	private static String removeZero(int[] arr) {
-			String result = "";
-			boolean zeros = true;
-			for (int i = 0; i < arr.length; i++) {
-					if (zeros && arr[i] == 0) {
-							result += "";
-
-					} else {
-							result += arr[i];
-							zeros = false;
-
-					}
-
+		String result = "";
+		boolean zeros = true;
+		for (int i = 0; i < arr.length; i++) {
+			if (zeros && arr[i] == 0) {
+				result += "";
+			} else {
+				result += arr[i];
+				zeros = false;
 			}
-			if (result.equals("")) {
-					return "0";
-
-			}
-			return result;
-
+		}
+		if (result.equals("")) {
+			return "0;
+		}
+		return result;
 	}
-
-
 }
+
